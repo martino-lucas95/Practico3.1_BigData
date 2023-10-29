c7a00c4011e6a13baba50db513e00cd4c644204e
martino-lucas95 <71530057+martino-lucas95@users.noreply.github.com>
2023-10-29 13:38:56 -0300

    Modify Paises_Mayor_Gasto.sql

--- Practico3/models/dev/Paises_Mayor_Gasto.sql
+++ Practico3/models/dev/Paises_Mayor_Gasto.sql
@@ -1,5 +1,7 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+
 SELECT
-    C.country as "Pais"
+    C.country as "Pais",
     ROUND(sum(I.total)) as "Total gastado"
 FROM {{source('ecommerce', 'customers')}} as C
 INNER JOIN {{source('ecommerce', 'invoices')}} as I ON C.customerid=I.customer
